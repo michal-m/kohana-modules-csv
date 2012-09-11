8657848424a74ebcf357587f4be8b13bf13dab2a
Michał Musiał <michal.j.musial@gmail.com>
2012-09-11 15:14:44 +0100

    adding CSV_Parser and CSV_Writer stub classes

---
+++ classes/CSV/CSV_Parser.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * The CSV_Parser class
+ *
+ * @package		CSV
+ * @author		Michał Musiał
+ * @copyright	(c) 2012 Michał Musiał
+ */
+class CSV_Parser
+{
+    
+}
---
+++ classes/CSV/CSV_Writer.php
@@ -0,0 +1,13 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+/**
+ * The CSV_Writer class
+ *
+ * @package		CSV
+ * @author		Michał Musiał
+ * @copyright	(c) 2012 Michał Musiał
+ */
+class CSV_Writer
+{
+
+}
